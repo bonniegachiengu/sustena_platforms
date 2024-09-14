be3167191acbb65a939f9b1e30656edd7093aeb1
bonniegachiengu <bonniegachiengu@gmail.com>
2024-09-14 20:57:57 +0300

    new sustena_platforms MVP trial

---
+++ cmd/sustena/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+    "github.com/bonniegachiengu/sustena_platforms/entropy/blockchain"
+    "github.com/bonniegachiengu/sustena_platforms/symmetry/vm"
+    "github.com/bonniegachiengu/sustena_platforms/embroidery/compiler"
+)
+
+func main() {
+    fmt.Println("Sustena Platform Initializing...")
+    // Initialize components here
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bonniegachiengu/sustena_platforms
+
+go 1.23.1
