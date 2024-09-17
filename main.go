eacde3bd115abbbb8820482cc8686ffcd9138ddc
bonniegachiengu <bonniegachiengu@gmail.com>
2024-09-17 09:06:49 +0300

    removed react app, need restart

--- main.go
+++ main.go
@@ -28,10 +28,6 @@ func main() {
 	http.HandleFunc("/buy_jul", server.BuyJULHandler)
 	http.HandleFunc("/get_mempool", server.GetMempoolHandler)
 
-	// Serve static files for the UI
-	fs := http.FileServer(http.Dir("./blockchain-ui/build"))
-	http.Handle("/", fs)
-
 	// Start the server
 	fmt.Println("Server starting on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
