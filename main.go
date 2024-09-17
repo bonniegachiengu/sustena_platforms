6d4ed219c796e1f4225c55d453c4299c4d41e327
bonniegachiengu <bonniegachiengu@gmail.com>
2024-09-17 12:14:42 +0300

    deleted and modified code to restart BC

--- main.go
+++ main.go
@@ -2,33 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"net/http"
-
-	"github.com/bonniegachiengu/sustena_platforms/entropy/blockchain"
-	"github.com/bonniegachiengu/sustena_platforms/api"
+	// Remove the unused import
+	// "sustena_platforms/entropy/blockchain"
 )
 
 func main() {
-	bc := blockchain.NewBlockchain()
-	ops := blockchain.NewBlockchainOperations(bc)
-
-	// Initialize API server
-	server := api.NewServer(api.APIConfig{
-		Blockchain: bc,
-		Operations: ops,
-	})
-
-	// Set up routes
-	http.HandleFunc("/create_account", server.CreateAccountHandler)
-	http.HandleFunc("/transfer", server.TransferHandler)
-	http.HandleFunc("/get_balance", server.GetBalanceHandler)
-	http.HandleFunc("/get_chain", server.GetChainHandler)
-	http.HandleFunc("/get_accounts", server.GetAccountsHandler)
-	http.HandleFunc("/buy_jul", server.BuyJULHandler)
-	http.HandleFunc("/get_mempool", server.GetMempoolHandler)
-
-	// Start the server
-	fmt.Println("Server starting on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	// Initialize your blockchain and other components here
+	fmt.Println("Sustena Platforms initialized")
 }
---
+++ utils/logger.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"time"
+)
+
+var (
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+)
+
+func init() {
+	file, err := os.OpenFile("sustena.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal("Failed to open log file:", err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
+
+type SustenaError struct {
+	Time    time.Time
+	Message string
+	File    string
+	Line    int
+}
+
+func (e *SustenaError) Error() string {
+	return fmt.Sprintf("[%s] %s in %s:%d", e.Time.Format(time.RFC3339), e.Message, e.File, e.Line)
+}
+
+func NewError(message string) error {
+	_, file, line, _ := runtime.Caller(1)
+	return &SustenaError{
+		Time:    time.Now(),
+		Message: message,
+		File:    file,
+		Line:    line,
+	}
+}
+
+func LogInfo(message string) {
+	InfoLogger.Println(message)
+}
+
+func LogError(err error) {
+	ErrorLogger.Println(err)
+}
